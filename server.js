e71524a92a92240ffe8dda6ca1b78024bc994302
phoenixyork166 <berlinlee2017@gmail.com>
2023-07-20 15:51:07 +0800

    fixing fetch3

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
