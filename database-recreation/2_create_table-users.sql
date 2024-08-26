ba4661f793daff09249cd78349c96808730cc717
berlinlee-phoenix <berlinlee2017@gmail.com>
2024-08-26 17:10:18 +0800

    responsiveness

--- database-recreation/2_create_table-users.sql
+++ database-recreation/2_create_table-users.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
 	id serial PRIMARY KEY,
-	name VARCHAR(100),
+	name varchar(100),
 	email text UNIQUE NOT NULL,
-	entries BIGINT DEFAULT 0,
-	joined TIMESTAMP NOT NULL,
-	raw_hex TEXT
+	entries bigint DEFAULT 0,
+	joined timestamp NOT NULL,
+	raw_hex text
 );
